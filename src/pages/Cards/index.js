fe990e2d5f683330979793c208a4c806ff3953b2
Kobe Rico <koberico@Kobes-MacBook-Pro.local>
2020-08-15 10:45:06 -0700

    Added cards design

---
+++ src/pages/Cards/index.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import Grid from '@material-ui/core/Grid';
+import { Link } from 'react-router-dom';
+
+
+import Cards from "./cards.json";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: "#316470",
+  },
+}));
+
+export default function InfoCard() {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Grid container justify="center" spacing={2}>
+    {/* All cards */ }
+        {
+    Cards.map(
+      (card, i) => (
+        <Grid item xs={12} md={6} lg={4} key={i}>
+          <Card className={classes.root}>
+            <CardHeader
+              avatar={
+                <Avatar aria-label="recipe" className={classes.avatar}>
+                  {card.avatar}
+                </Avatar>
+              }
+              // action={
+              //   <IconButton aria-label="settings">
+              //     <MoreVertIcon />
+              //   </IconButton>
+              // }
+              title={card.title}
+              subheader={card.subheader}
+            />
+            <CardMedia
+              className={classes.media}
+              image={card.image}
+              title=""
+            />
+            <CardContent>
+              <Typography variant="body2" color="textSecondary" component="p">
+                {card.content}
+              </Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+              <IconButton aria-label="add to favorites">
+                <FavoriteIcon />
+              </IconButton>
+              <IconButton aria-label="share">
+                <ShareIcon />
+              </IconButton>
+              <IconButton
+                className={clsx(classes.expand, {
+                  [classes.expandOpen]: expanded,
+                })}
+                onClick={handleExpandClick}
+                aria-expanded={expanded}
+                aria-label="show more"
+              >
+                <ExpandMoreIcon />
+              </IconButton>
+            </CardActions>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+              <CardContent>
+                <Typography paragraph>
+                  {card.expand}
+                </Typography>
+
+              </CardContent>
+            </Collapse>
+          </Card>
+        </Grid>
+      )
+    )
+  }
+  </Grid>
+  )
+}
+
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { withAuthorization } from '../../services/Session';
+import BrandCardHeader from '../Cards';
+
 
 const HomePage = () => (
   <div>
     <h1>Home</h1>
+    <BrandCardHeader/>
     <p>The Home Page is accessible by every signed in user.</p>
   </div>
 );
