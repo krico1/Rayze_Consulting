235a529aab34827693e4580ea07b40b6715f904b
Kobe Rico <koberico@Kobes-MBP.lan>
2020-08-27 08:57:59 -0700

    deleted

--- src/App/index.js
+++ src/App/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import {
   BrowserRouter as Router,
   Route,
@@ -10,83 +10,96 @@ import {
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from '../components/MUITheme/theme.js';
-
-import Navigation from '../components/Navigation';
-import LandingPage from '../components/Landing';
-import SignUpPage from '../pages/RegisterPages/SignUp';
-import SignInPage from '../pages/RegisterPages/SignIn';
-import PasswordForgetPage from '../pages/PasswordForget';
-import HomePage from '../pages/Home';
-import AccountPage from '../pages/Account';
-import InfoPage from '../pages/Info';
+import ProgressCircle from "../components/ProgressCircle";
 
 import Header from "../components/Header";
-import {HomeHeader, SignInHeader, SignUpHeader, InfoHeader, AccountHeader} from "../components/Header";
+import { HomeHeader, SignInHeader, SignUpHeader, InfoHeader, AccountHeader } from "../components/Header";
 
 // import AdminPage from '../Admin';
- 
+
 import * as ROUTES from '../constants/routes';
- 
+
 import { withAuthentication } from '../services/Session';
+
+const Navigation = lazy(() => import('../components/Navigation'));
+const LandingPage = lazy(() => import('../components/Landing'));
+const SignUpPage = lazy(() => import('../pages/RegisterPages/SignUp'));
+const SignInPage = lazy(() => import('../pages/RegisterPages/SignIn'));
+const PasswordForgetPage = lazy(() => import('../pages/PasswordForget'));
+const HomePage = lazy(() => import('../pages/Home'));
+const AccountPage = lazy(() => import('../pages/Account'));
+const InfoPage = lazy(() => import('../pages/Info'));
 
 const App = () => (
   <ThemeProvider theme={theme}>
     <CssBaseline />
     <Router>
-    <div>
-    <Switch>
-        <Route
-          exact
-          path="/account"
-          component={AccountHeader}
-        />
-        <Route
-          exact
-          path="/home"
-          component={HomeHeader}
-        />
-        <Route
-          exact
-          path="/info"
-          component={InfoHeader}
-        />
-        <Route
-          exact
-          path="/landing"
-          component={Header}
-        />
-        <Route
-          exact
-          path="/signin"
-          component={SignInHeader}
-        />
-        <Route
-          exact
-          path="/signout"
-          component={SignUpHeader}
-        />
-      </Switch>
-      {/* <Header/> */}
-      <Navigation />
- 
-      <hr />
- 
-      <Route exact path={ROUTES.LANDING} component={LandingPage} />
-      <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-      <Route
-        path={ROUTES.PASSWORD_FORGET}
-        component={PasswordForgetPage}
-      />
-      <Route path={ROUTES.HOME} component={HomePage} />
-      <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-      <Route path={ROUTES.INFO} component={InfoPage} />
+      <div>
+        <Switch>
+          <Route
+            exact
+            path="/account"
+            component={AccountHeader}
+          />
+          <Route
+            exact
+            path="/home"
+            component={HomeHeader}
+          />
+          <Route
+            exact
+            path="/info"
+            component={InfoHeader}
+          />
+          <Route
+            exact
+            path="/landing"
+            component={Header}
+          />
+          <Route
+            exact
+            path="/signin"
+            component={SignInHeader}
+          />
+          <Route
+            exact
+            path="/signout"
+            component={SignUpHeader}
+          />
+        </Switch>
+        {/* <Header/> */}
+        
+        <Navigation />
 
-      {/* <Route path={ROUTES.ADMIN} component={AdminPage} /> */}
-    </div>
-  </Router>
+        {/* <hr /> */}
+      
+          <Suspense
+            fallback={
+              <ProgressCircle />
+            }>
+              <Switch>
+
+                 
+              <Route exact path={ROUTES.LANDING} component={LandingPage} />
+              <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+              <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+              <Route
+                path={ROUTES.PASSWORD_FORGET}
+                component={PasswordForgetPage}
+              />
+              <Route path={ROUTES.HOME} component={HomePage} />
+              <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+              <Route path={ROUTES.INFO} component={InfoPage} />
+              </Switch>
+          </Suspense>
+
+
+      
+          
+      </div>
+    </Router>
   </ThemeProvider>
-  
+
 );
- 
+
 export default withAuthentication(App);
---
+++ src/components/ProgressCircle/index.js
@@ -0,0 +1,20 @@
+// THIS SPINNING PROGRESS CIRCLE SHOWS AS A FALLBACK FOR LAZYING LOADING SUSPENSE
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: "2rem",
+  },
+}));
+
+export default function CircularIndeterminate() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CircularProgress />
+    </div>
+  );
+}
